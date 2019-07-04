0e78b811f2957cd90876689ec653c248c00c37bc
sonjy <thswldus1994@naver.com>
2019-07-04 18:20:22 +0900

    vue.js prop form

--- nodeapp/views/vue/js/component/vue_form.js
+++ nodeapp/views/vue/js/component/vue_form.js
@@ -1,63 +1,27 @@
-Vue.component('child', {
-	props: ['whatdidyousay'],
-	template: '<span>{{ whatdidyousay }}</span>'
-});
-new Vue({
-	el: '#prop01'
-});
-
-
-Vue.component('jiyeon', {
-	props: ['myMessage'],
-	template: '<span>{{ myMessage }}</span>'
-});
-new Vue({
-	el: '#prop02'
-});
-
-
-Vue.component('child', {
-	props: ['myMessage'],
-	template: '<span>{{ myMessage }}</span>'
-});
-new Vue({
-	el: '#prop03',
-	data: {
-		parentMsg: ''
-	}
-});
-
-
-new Vue({
-	el: '#prop04',
-	data: {
-		types : ['String', 'Number', 'Boolean', 'Function', 'Object', 'Array', 'Symbol']
-	}
-});
-
-
-Vue.component('button-counter', {
-	template: '<button v-on:click="incrementCounter">{{ counter }}</button>',
-	data: function() {
-		return {
-			counter: 0
-		}
-	},
+Vue.component('currency-input', {
+	template: '\
+		<span>\
+			$\
+			<input\
+				ref="input"\
+				v-bind:value="value"\
+				v-on:input="updateValue($event.target.value)">\
+		</span>\
+	',
+	props: ['value'],
 	methods: {
-		incrementCounter: function() {
-			this.counter++;
-			this.$emit('increment');
+		// 값을 직접 업데이트 하는 대신 이 메소드를 사용하여 입력 값에 대한 서식을 지정할 수 있습니다.
+		updateValue: function(value) {
+			var formattedValue = value.trim().slice(0, value.indexOf('.') === -1 ? value.length : value.indexOf('.') + 3)
+
+			if (formattedValue !== value) {
+				this.$refs.input.value = formattedValue;
+			}
+
+			this.$emit('input', Number(formattedValue));
 		}
 	}
 });
 new Vue({
-	el: '#counter-event-example',
-	data: {
-		total: 0
-	},
-	methods: {
-		incrementTotal: function() {
-			this.total++;
-		}
-	}
+	'el' : '#form01'
 });
