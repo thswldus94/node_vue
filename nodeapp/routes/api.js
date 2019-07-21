be58efd7f156471da0f429401a580675975c54ad
sonjy <thswldus1994@naver.com>
2019-07-22 00:40:44 +0900

    login logout get session module

--- nodeapp/routes/api.js
+++ nodeapp/routes/api.js
@@ -5,7 +5,6 @@ var router = express.Router();
 
 var passport = require('passport');
 var flash = require('connect-flash');
-console.log('탔냐?');
 // 이미 app.js에서 /api 에 대한 라우터 정의를 해 주었음
 // 따라서 /api/login 으로 안쓰고 그냥 /login 만 써도 됨
 router.post('/login', function(req, res, next) {
@@ -14,13 +13,41 @@ router.post('/login', function(req, res, next) {
         if (info.state === true) {
             req.logIn(user, function(err) {
                 if (err) { return next(err); }
-                return res.redirect('/login');
+                //return res.redirect('/');
+                res.send({state: true});
+                return next();
             });
         } else {
-            res.send(info.message);
+            res.send({
+                state: false,
+                message: info.message
+            });
             return next(err);
         }
     })(req, res, next);
 });
+
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+router.get('/session', ensureAuthenticated, function(req, res) {
+  // deserilizUser에서 추가로 저장한 정보까지 받음
+  var userInfo = req.user;
+  console.log(userInfo);
+  res.json(userInfo);
+});
+
+function ensureAuthenticated(req, res, next) {
+  console.log(req);
+  if (req.isAuthenticated()) { // 현재 session이 유효한 세션인지
+      // 유효하면 다음으로
+      return next();
+  }
+  // 유효하지 않은 경우
+  res.status(401);
+  res.json({});
+}
 
 module.exports = router;
